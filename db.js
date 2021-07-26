292c0253878b6364d7176db0dfa047ebad67c33e
MilesTheEdgy <mohammad.nadom98@gmail.com>
2021-07-26 23:48:12 +0300

    asdasdsad

--- db.js
+++ db.js
@@ -1,18 +1,22 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  host: "localhost",
-  user: 'postgres',
-  password: "crossmyheart1243",
-  port: 5432,
-  database: "eczane",
-})
+//this is my LOCALHOST
 
 // const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+//   host: "localhost",
+//   user: 'postgres',
+//   password: "crossmyheart1243",
+//   port: 5432,
+//   database: "eczane",
+// })
+
+//this is my SERVER ENVIORMENT VAR
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
