7ea7e8a51eb1a496836d67346a5c50a2a305b1a4
Muhammed <mohammad.nadom98@gmail.com>
2021-09-30 09:33:38 +0000

    changed default port to 8080

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
       
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`)
 })
